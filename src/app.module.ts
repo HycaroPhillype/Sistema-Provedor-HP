f0bbd681c447a70a528763194f1309f8c1500f8c
Hycaro Phillype <phillypehycaro@gmail.com>
2025-07-06 12:21:07 -0300

    feat: config terminado dos aquivos docker-compose.yml e .env para conexão doker e postgres. Conexao com sucesso

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { ClientesModule } from './clientes/clientes.module';
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 5432,
-      username: 'proverdor_hp',
-      password: 'proverdor_hp',
-      database: 'proverdor_db',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
