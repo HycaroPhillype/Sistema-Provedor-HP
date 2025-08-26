502b4c717dbe90d8414ba13bfea50d2f78e928c9
Hycaro Phillype <phillypehycaro@gmail.com>
2025-08-26 14:10:24 -0300

    Resolvendo erros no passport-jwt

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,38 @@
+import { ExtractJwt, JwtFromRequestFunction, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsuarioService } from '../../usuarios/usuarios.service';
+
+export interface JwtPayload {
+  sub: number;
+  email: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private usuarioService: UsuarioService) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const jwtExtractor: (req: Request) => string | null =
+      ExtractJwt.fromAuthHeaderAsBearerToken() as JwtFromRequestFunction<string>;
+
+    super({
+      jwtFromRequest: jwtExtractor,
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET || 'secretKey',
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.usuarioService.searchId(payload.sub);
+
+    if (!user) {
+      throw new UnauthorizedException('Usuário não encontrado');
+    }
+
+    if (!user.ativo) {
+      throw new UnauthorizedException('Usuário inativo');
+    }
+
+    return user;
+  }
+}
