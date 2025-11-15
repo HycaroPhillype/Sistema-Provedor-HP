223c69f2266da441fb8f073fd6b894746c9736a1
Hycaro Phillype <phillypehycaro@gmail.com>
2025-11-15 11:14:33 -0300

    feat: Criado o Módulo de Pagamentos

---
+++ src/pagamentos/pagamentos.mudule.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PagamentosService } from './pagamentos.service';
+import { PagamentosController } from './pagamentos.controller';
+import { TransacaoPagamento } from './entities/transacao-pagamento.entity';
+import { Fatura } from '../financeiro/entities/fatura.entity';
+import { Cliente } from '../clientes/entities/cliente.entity';
+import { HistoricoModule } from '../historico/historico.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([TransacaoPagamento, Fatura, Cliente]),
+    HistoricoModule,
+  ],
+  controllers: [PagamentosController],
+  providers: [PagamentosService],
+  exports: [PagamentosService],
+})
+export class PagamentosModule {}
