462e767d819b3b22808aed311b69197bff2d1439
Hycaro Phillype <phillypehycaro@gmail.com>
2025-11-21 14:06:00 -0300

    feat: Criando relatorios financeiros detalhados. Dto(filtro-relatorio.dto.ts) finalizadogit add .

---
+++ src/relatorios/dto/filtro-relatorios.dto.ts
@@ -0,0 +1,30 @@
+import { IsOptional, IsDateString, IsEnum, IsNumber } from 'class-validator';
+
+export enum TipoRelatorio {
+  RESUMO_MENSAL = 'resumo_mensal',
+  CLIENTE_INADIMPLENTE = 'cliente_inadimplente',
+  RECEITAS_PLANOS = 'receitas_planos',
+  HISTORICO_PAGAMENTOS = 'historico_pagamentos',
+}
+
+export class FiltroRelatoriosDto {
+  @IsOptional()
+  @IsDateString()
+  dataInicio?: string;
+
+  @IsOptional()
+  @IsDateString()
+  dataFim?: string;
+
+  @IsOptional()
+  @IsEnum(TipoRelatorio)
+  tipoRelatorio: TipoRelatorio;
+
+  @IsOptional()
+  @IsNumber()
+  clienteId?: number;
+
+  @IsOptional()
+  @IsNumber()
+  planoId?: number;
+}
